3081b2b9dfc691b0e69ffc7434a5b4c54d6213ec
Arpit <arpittale123@gmail.com>
2026-09-06 00:10:34 +0530

    Add solution for 2894. Divisible and Non-divisible Sums Difference [Medium] | LeetSync

--- Medium/2894-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.cpp
+++ Medium/2894-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int differenceOfSums(int n, int m) {
-        int sum=0,num=0;
-        for(int i=1;i<=n;i++){
-            if(i%m!=0){
-                sum+=i;
-            }
-        }
-        for(int i=1;i<=m;i++){
-            if(i%m!=0){
-                num+=i;
-            }
-        }
-        return sum-num;
+        int sum = 0, num = 0;
         
+        for(int i = 1; i <= n; i++) {
+            if(i % m != 0) sum += i;
+        }
+        
+        for(int i = 1; i <= m; i++) {
+            if(i % n != 0) num += i;
+        }
+        
+        return abs(sum - num);
     }
 };
+
