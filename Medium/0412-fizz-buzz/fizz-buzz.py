f435315aa94554c93ce48678106aa6382ee0ca4d
Arpit <arpittale123@gmail.com>
2026-09-06 23:55:15 +0530

    Add solution for 0412. Fizz Buzz [Medium] | LeetSync

---
+++ Medium/0412-fizz-buzz/fizz-buzz.py
@@ -0,0 +1,20 @@
+        for(int i=1;i<=n;i++){
+            if(i%3==0 && i%5==0){
+                ans.push_back("FizzBuzz");
+            }
+            else if(i%3==0){
+                ans.push_back("Fizz");
+            }
+            else if(i%5==0){
+                ans.push_back("Buzz");
+            }
+            else{
+                ans.push_back(to_string(i));
+            }
+        }
+        return ans;
+        vector<string> ans;
+    vector<string> fizzBuzz(int n) {
+public:
+class Solution {
+
